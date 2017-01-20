2c8cb84c47e60891398712c432254011484abcd1
britth <brittmhayes@gmail.com>
2017-01-20 15:35:40 -0500

    Add class method to read csv row by row and test

--- user_validator.rb
+++ user_validator.rb
@@ -1,4 +1,25 @@
+require 'csv'
+
 class UserValidator
-  def initialize
+  attr_reader :data
+  def initialize(filename)
+    @all_rows = []
+    @header = []
+    @count = 0
+    @data = CSV.foreach(filename) do |row|
+              if $INPUT_LINE_NUMBER == 1
+                @header << row
+              else
+                all_rows << row
+              end
+            end
+  end
+
+  def header
+    @header
+  end
+
+  def all_rows
+    @all_rows
   end
 end
--- user_validator_test.rb
+++ user_validator_test.rb
@@ -3,5 +3,15 @@ require 'minitest/pride'
 require './user_validator.rb'
 
 class UserValidatorTest < Minitest::Test
-  
+  def test_initialize
+    assert UserValidator.new('homework.csv')
+  end
+
+  def test_correct_number_of_data_rows_are_returned
+    u = UserValidator.new('homework.csv')
+
+    assert_equal(u.all_rows.count, 7)
+    assert_equal(u.header.count, 1)
+  end
+
 end
