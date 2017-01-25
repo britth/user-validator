1a5c7860c5e8fa56f4bb2da98520e8e3c8422b51
britth <brittmhayes@gmail.com>
2017-01-25 16:13:40 -0500

    Add regex to check for invalid join dates and test

--- user_validator.rb
+++ user_validator.rb
@@ -43,18 +43,31 @@ class UserValidator
     @all_rows.select{|x| invalid_age?(x)}.map{|x| [x.to_s, errors(x)]}
   end
 
+  def invalid_join_date_rows
+    @all_rows.select{|x| invalid_date?(x)}.map{|x| [x.to_s, errors(x)]}
+  end
+
   def invalid?(row)
-    invalid_phone?(row) || invalid_age?(row)
+    invalid_phone?(row) || invalid_age?(row) || invalid_date?(row)
   end
 
   def invalid_phone?(row)
-    row.at(header.first.index('phone')).match(/^(?:|[\(])[1-9]\d{2}(?:[\)] |[\)]|-|.|)[1-9]\d{2}(?:-|.|)\d{4}$/).nil?
+    phone = row.at(header.first.index('phone'))
+    phone.
+      match(/^(?:|[\(])[1-9]\d{2}(?:[\)] |[\)]|-|.|)[1-9]\d{2}(?:-|.|)\d{4}$/).
+      nil?
   end
 
   def invalid_age?(row)
     age = row.at(header.first.index('age'))
     is_not_number?(age) || age.to_s.match(/^[1]?[0-9][0-9]$/).nil?
   end
+
+  def invalid_date?(row)
+    date = row.at(header.first.index('joined'))
+    date.match(/^([0][1-9]|[1][0-2]|\d{1})[-\/.]([0-2][0-9]|[3][0-1]|\d{1})[-\/.](\d{2}|\d{4})$/).nil? && date.match(/^\d{4}[-\/.]([0][1-9]|[1][0-2]|\d{1})[-\/.]([0-2][0-9]|[3][0-1]|\d{1})$/).nil?
+  end
+
 
   def errors(row)
     errors = []
@@ -64,12 +77,14 @@ class UserValidator
     if invalid_phone?(row)
       errors << 'Invalid phone'
     end
+    if invalid_date?(row)
+      errors << 'Invalid date'
+    end
     errors
   end
 
 end
 
-u = UserValidator.new('homework.csv')
-
+# u = UserValidator.new('homework.csv')
+#
 # puts u.invalid_rows
-# # puts u.all_rows.map{|x| x[2]}
--- user_validator_test.rb
+++ user_validator_test.rb
@@ -3,26 +3,36 @@ require 'minitest/pride'
 require './user_validator.rb'
 
 class UserValidatorTest < Minitest::Test
+
+
+  def setup
+    @u = UserValidator.new('homework.csv')
+  end
+
   def test_initialize
     assert UserValidator.new('homework.csv')
   end
 
   def test_correct_number_of_data_rows_are_returned
-    u = UserValidator.new('homework.csv')
+    #u = UserValidator.new('homework.csv')
 
-    assert_equal(u.all_rows.count, 7)
-    assert_equal(u.header.count, 1)
+    assert_equal(@u.all_rows.count, 7)
+    assert_equal(@u.header.count, 1)
   end
 
   def test_correct_number_of_invalid_rows_based_on_phone
-    u = UserValidator.new('homework.csv')
+    #u = UserValidator.new('homework.csv')
 
-    assert_equal(u.invalid_phone_rows.count, 3)
+    assert_equal(@u.invalid_phone_rows.count, 3)
   end
 
   def test_correct_number_of_invalid_rows_based_on_age
-    u = UserValidator.new('homework.csv')
+    #u = UserValidator.new('homework.csv')
 
-    assert_equal(u.invalid_age_rows.count, 0)
+    assert_equal(@u.invalid_age_rows.count, 0)
+  end
+
+  def test_correct_number_of_invalid_rows_based_on_join_date
+    assert_equal(@u.invalid_join_date_rows.count, 4)
   end
 end
